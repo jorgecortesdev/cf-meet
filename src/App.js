cc626b6964215ff27bca1fc45741bc84f814f1b1
Jorge Cortes <jorge.cortes@gmail.com>
2021-04-17 15:03:07 -0700

    Added integration test, a user can select a city from the suggested list.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
+export default class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <NumberOfEvents />
-      <EventList />
-    </div>
-  );
+  componentDidMount() {
+    this.mounted = true;
+
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({
+          events: events,
+          locations: extractLocations(events)
+        });
+      }
+    });
+  };
+
+  componentWillUnmount() {
+    this.mounted = false;
+  };
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents,
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <NumberOfEvents />
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
 }
-
-export default App;
