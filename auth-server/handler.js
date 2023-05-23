93f66360b832ca208e5abf47358856f2bbdc993d
Jorge Cortés <jorge.cortes@gmail.com>
2023-05-22 19:51:44 -0700

    fixed javascript origins and redirect uris

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -13,8 +13,8 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://xorth.github.io/meet/"],
-  javascript_origins: ["https://xorth.github.io", "http://localhost:3000"],
+  redirect_uris: ["https://jorgecortesdev.github.io/cf-meet/"],
+  javascript_origins: ["https://jorgecortesdev.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
